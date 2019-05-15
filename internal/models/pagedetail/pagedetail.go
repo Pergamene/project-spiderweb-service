36e28211e876a28dc775d68d811c28c451bdafef
Ryan Saunders <rsaunders@qualtrics.com>
2019-05-15 11:07:00 -0600

    adding guid for detail

--- internal/models/pagedetail/pagedetail.go
+++ internal/models/pagedetail/pagedetail.go
@@ -52,6 +52,8 @@ func GetPermissionType(propertyTypeString string) (PartitionType, error) {
 
 // PageDetail is a single detail for a page.
 type PageDetail struct {
+	ID         int64       `json:"-"`
+	GUID       string      `json:"id"`
 	Title      string      `json:"title"`
 	Summary    string      `json:"summary"`
 	Partitions []Partition `json:"partitions"`
