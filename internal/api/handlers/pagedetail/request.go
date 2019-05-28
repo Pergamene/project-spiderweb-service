29e05aaf4b27c642e476c87900571e768261fea2
Ryan Saunders <rsaunders@qualtrics.com>
2019-05-27 18:30:10 -0600

    should work, haven't tested with actually unmarshalling the request, but that should be done as part of the pagedetail handler test anyway.

--- internal/api/handlers/pagedetail/request.go
+++ internal/api/handlers/pagedetail/request.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/Pergamene/project-spiderweb-service/internal/models/pagedetail"
 	"github.com/julienschmidt/httprouter"
 	"github.com/pkg/errors"
 )
@@ -12,9 +13,9 @@ import (
 type UpdatePageDetailRequest struct {
 	PageGUID       string
 	PageDetailGUID string
-	Title          string                   `json:"title"`
-	Summary        string                   `json:"summary"`
-	Partitions     []map[string]interface{} `json:"partitions"`
+	Title          string                 `json:"title"`
+	Summary        string                 `json:"summary"`
+	Partitions     []pagedetail.Partition `json:"partitions"`
 }
 
 // NewUpdatePageDetailRequest extracts the UpdatePageDetailRequest
@@ -23,6 +24,10 @@ func NewUpdatePageDetailRequest(r *http.Request, p httprouter.Params) (UpdatePag
 	err := json.NewDecoder(r.Body).Decode(&request)
 	if err != nil {
 		return request, errors.New("invalid request")
+	}
+	err = pagedetail.UnmarshalPartitions(request.Partitions)
+	if err != nil {
+		return request, errors.New("not valid page partitions")
 	}
 	request.PageGUID = p.ByName(PageIDRouteKey)
 	request.PageDetailGUID = p.ByName(PageDetailIDRouteKey)
--- internal/models/pagedetail/pagedetail.go
+++ internal/models/pagedetail/pagedetail.go
@@ -1,76 +1,5 @@
 package pagedetail
 
-import "github.com/pkg/errors"
-
-// PartitionType is a valid property type.
-type PartitionType string
-
-// All the valid values for PartitionType
-const (
-	PartitionTypeHeaderOne     PartitionType = "h1"
-	PartitionTypeHeaderTwo     PartitionType = "h2"
-	PartitionTypeHeaderThree   PartitionType = "h3"
-	PartitionTypeHeaderFour    PartitionType = "h4"
-	PartitionTypeHeaderFive    PartitionType = "h5"
-	PartitionTypeHeaderSix     PartitionType = "h6"
-	PartitionTypeParagraph     PartitionType = "p"
-	PartitionTypeUnorderedList PartitionType = "ul"
-	PartitionTypeOrderedList   PartitionType = "ol"
-	PartitionTypeImage         PartitionType = "image"
-	PartitionTypeQuotes        PartitionType = "quotes"
-	PartitionTypePageBreak     PartitionType = "hr"
-	PartitionTypeText          PartitionType = "text"
-	PartitionTypeBold          PartitionType = "bold"
-	PartitionTypeItalics       PartitionType = "italics"
-	PartitionTypeLink          PartitionType = "link"
-	PartitionTypeRelation      PartitionType = "relation"
-	PartitionTypeColor         PartitionType = "color"
-)
-
-// GetPartitionType returns the correct permission type for the given string.
-func GetPartitionType(propertyTypeString string) (PartitionType, error) {
-	switch propertyTypeString {
-	case string(PartitionTypeHeaderOne):
-		return PartitionTypeHeaderOne, nil
-	case string(PartitionTypeHeaderTwo):
-		return PartitionTypeHeaderTwo, nil
-	case string(PartitionTypeHeaderThree):
-		return PartitionTypeHeaderThree, nil
-	case string(PartitionTypeHeaderFour):
-		return PartitionTypeHeaderFour, nil
-	case string(PartitionTypeHeaderFive):
-		return PartitionTypeHeaderFive, nil
-	case string(PartitionTypeHeaderSix):
-		return PartitionTypeHeaderSix, nil
-	case string(PartitionTypeParagraph):
-		return PartitionTypeParagraph, nil
-	case string(PartitionTypeUnorderedList):
-		return PartitionTypeUnorderedList, nil
-	case string(PartitionTypeOrderedList):
-		return PartitionTypeOrderedList, nil
-	case string(PartitionTypeImage):
-		return PartitionTypeImage, nil
-	case string(PartitionTypeQuotes):
-		return PartitionTypeQuotes, nil
-	case string(PartitionTypePageBreak):
-		return PartitionTypePageBreak, nil
-	case string(PartitionTypeText):
-		return PartitionTypeText, nil
-	case string(PartitionTypeBold):
-		return PartitionTypeBold, nil
-	case string(PartitionTypeItalics):
-		return PartitionTypeItalics, nil
-	case string(PartitionTypeLink):
-		return PartitionTypeLink, nil
-	case string(PartitionTypeRelation):
-		return PartitionTypeRelation, nil
-	case string(PartitionTypeColor):
-		return PartitionTypeColor, nil
-	default:
-		return PartitionTypeText, errors.Errorf("invalid property type %v", propertyTypeString)
-	}
-}
-
 // PageDetail is a single detail for a page.
 type PageDetail struct {
 	ID         int64       `json:"-"`
@@ -78,16 +7,4 @@ type PageDetail struct {
 	Title      string      `json:"title"`
 	Summary    string      `json:"summary"`
 	Partitions []Partition `json:"partitions"`
-}
-
-// Partition is a single markdown partition for a detail.
-type Partition struct {
-	Type       PartitionType `json:"type"`
-	Value      string        `json:"value,omitempty"`
-	Partitions []Partition   `json:"partitions,omitempty"`
-	Items      []Partition   `json:"items,omitempty"`
-	AltText    string        `json:"altText,omitempty"`
-	Link       string        `json:"link,omitempty"`
-	Relation   string        `json:"relation,omitempty"`
-	Color      string        `json:"color,omitempty"`
 }
